de5d6a939415e8f5da71014a94061400f1513bd6
jdnricardo <julian.ricardo376@gmail.com>
2024-12-02 16:14:57 -0500

    Add comments

--- R/day-01.R
+++ R/day-01.R
@@ -2,14 +2,20 @@ library(here)
 library(tidytable)
 library(janitor)
 
+# Find where I downloaded file
 here("data", "input-day01.txt") %>% 
+# Read in as single column
   read.delim(header = FALSE,
              col.names = c("lists")) %>% 
-  remove_empty("cols") %>% 
+  # Separate into two columns
   separate("lists", paste0("list_", 1:2)) %>% 
+  # Convert type
   map(as.integer) %>%
+  # Sort
   map(sort) %>% 
+  # Recreate data.frame structure
   bind_cols() %>% 
+  # Compute distance
   summarise(
     distance = sum(abs(list_1 - list_2))
   )
